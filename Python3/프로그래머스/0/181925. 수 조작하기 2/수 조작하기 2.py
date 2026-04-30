98f38381d769158143ef3fa7a3d2be79afe6e238
Hyeyeon Kim <132979277+chyoang30@users.noreply.github.com>
2026-04-30 22:36:43 +0900

    [level 0] Title: 수 조작하기 2, Time: 17.47 ms, Memory: 13.1 MB -BaekjoonHub

---
+++ Python3/프로그래머스/0/181925. 수 조작하기 2/수 조작하기 2.py
@@ -0,0 +1,12 @@
+def solution(numLog):
+    answer = ''
+    
+    for i in range(0, len(numLog)-1):
+        x = numLog[i+1]-numLog[i]
+        
+        if x==1: answer += 'w'
+        elif x==-1: answer += 's'
+        elif x==10: answer += 'd'
+        else: answer += 'a'
+    
+    return answer
