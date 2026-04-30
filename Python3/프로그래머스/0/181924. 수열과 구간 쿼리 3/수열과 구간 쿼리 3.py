5636b1b8786465c968239b2a2e6a9b0ea2360c9a
Hyeyeon Kim <132979277+chyoang30@users.noreply.github.com>
2026-05-01 00:41:30 +0900

    Simplify query iteration using tuple unpacking

--- Python3/프로그래머스/0/181924. 수열과 구간 쿼리 3/수열과 구간 쿼리 3.py
+++ Python3/프로그래머스/0/181924. 수열과 구간 쿼리 3/수열과 구간 쿼리 3.py
@@ -1,7 +1,5 @@
 def solution(arr, queries):
-    for x in range(len(queries)):
-        i = queries[x][0]
-        j = queries[x][1]
+    for i,j in queries:
         arr[i], arr[j] = arr[j], arr[i]
     
     return arr
