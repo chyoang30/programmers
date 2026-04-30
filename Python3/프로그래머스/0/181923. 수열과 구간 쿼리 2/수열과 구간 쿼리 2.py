d962a4358bbed163f0fc96e50eb92b3b875c116e
Hyeyeon Kim <132979277+chyoang30@users.noreply.github.com>
2026-05-01 00:59:52 +0900

    Fix threshold value for query results

--- Python3/프로그래머스/0/181923. 수열과 구간 쿼리 2/수열과 구간 쿼리 2.py
+++ Python3/프로그래머스/0/181923. 수열과 구간 쿼리 2/수열과 구간 쿼리 2.py
@@ -1,11 +1,11 @@
 def solution(arr, queries):
     answer = []
     for s, e, k in queries:
-        num = 1000000
+        num = 1000001
         for i in range(s, e+1):
             if arr[i]>k and arr[i]<num:
                 num = arr[i]
-        if num==1000000: num=-1
+        if num==1000001: num=-1
         answer.append(num)
     
     return answer
