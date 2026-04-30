c5207618544f2ddfb354154f2a9dc41cda08085e
Hyeyeon Kim <132979277+chyoang30@users.noreply.github.com>
2026-05-01 00:57:28 +0900

    [level 0] Title: 수열과 구간 쿼리 2, Time: 11.58 ms, Memory: 9.39 MB -BaekjoonHub

---
+++ Python3/프로그래머스/0/181923. 수열과 구간 쿼리 2/수열과 구간 쿼리 2.py
@@ -0,0 +1,11 @@
+def solution(arr, queries):
+    answer = []
+    for s, e, k in queries:
+        num = 1000000
+        for i in range(s, e+1):
+            if arr[i]>k and arr[i]<num:
+                num = arr[i]
+        if num==1000000: num=-1
+        answer.append(num)
+    
+    return answer
