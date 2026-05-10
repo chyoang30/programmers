7242a211e9611ae35c9770673fbd309614096c97
Hyeyeon Kim <132979277+chyoang30@users.noreply.github.com>
2026-05-10 11:52:11 +0900

    Refactor signal processing and improve readability
    Refactor signal processing logic to use 'period' vector instead of 'cycle'. Added comments for clarity and improved readability.

--- C++/프로그래머스/1/468371. 노란불 신호등/노란불 신호등.cpp
+++ C++/프로그래머스/1/468371. 노란불 신호등/노란불 신호등.cpp
@@ -15,39 +15,48 @@ long long lcm(long long a, long long b){
 
 int solution(vector<vector<int>> signals) {
     int size = signals.size();
-    vector<int> cycle;
+    vector<int> period;
     
     for(int i=0;i<signals.size();i++){
-        cycle.push_back(signals[i][0] + signals[i][1] + signals[i][2]);
+        period.push_back(signals[i][0] + signals[i][1] + signals[i][2]);
     }
     
     long long total_lcm = 1;
-    for(int i=0;i<cycle.size();i++){
-            total_lcm = lcm(total_lcm, cycle[i]);
+    for(int i=0;i<period.size();i++){
+            total_lcm = lcm(total_lcm, period[i]);
     }
     
-    int g0 = signals[0][0], y0 = signals[0][1];
-    int p0 = cycle[0];
+    int g0 = signals[0][0], y0 = signals[0][1]; // 2, 3
+    int p0 = period[0]; // 7
     
-    for(int t=1;t<=total_lcm;t++){
-        vector<bool> isYellow(signals.size());
-        for(int i=0;i<signals.size();i++){
-            int p = t%cycle[i];
-            if(p==0) p==cycle[i];
-            if(p>signals[i][0]&&p<=signals[i][0]+signals[i][1])
-                isYellow[i] = true;
-            else isYellow[i] = false;
-        }
-        bool allYellow = true;
-        for(int i=0;i<signals.size();i++){
-            if(!isYellow[i]){
-                allYellow = false;
-                break;
+    /*
+    [testcase 2]
+    신호등 0: G=2, Y=3, R=2 → 주기 7
+    t:   1   2   3   4    5   6   7
+        🟢  🟢  🟡  🟡  🟡  🔴  🔴
+
+    t:   8   9   10  11  12   13  14
+        🟢  🟢  🟡  🟡  🟡  🔴  🔴
+    
+    노란불 시각: 3, 4, 5, 10, 11, 12 ...
+    */
+    
+    for(int c=0; ;c++){ // c: 몇 번째 주기?
+        for(int offset=0;offset<y0;offset++){   // offset: 노란 불 중 몇 번째 초?
+            
+            long long t = c * p0 + g0 + offset + 1; // 지금 시간이자 신호등0이 노란불인 시각
+            
+            if(t>total_lcm) return -1; // 범위 넘어가면 종료
+            
+            bool allYellow = true;
+            for(int i=0;i<signals.size();i++){
+                int p = (t-1)%period[i];
+                if(!(p>=signals[i][0]&&p<signals[i][0]+signals[i][1])){
+                    allYellow = false;
+                    break;
+                }
             }
+            if(allYellow) return (int)t;
         }
-        
-        if(allYellow) return t;
     }
- 
-    return -1;
 }
