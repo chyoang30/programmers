966db190449497f6fd4fc680ed0fc8f8029af211
Hyeyeon Kim <132979277+chyoang30@users.noreply.github.com>
2026-05-13 12:47:00 +0900

    [level 1] Title: 소수 만들기, Time: 44.36 ms, Memory: 4.19 MB -BaekjoonHub

---
+++ C++/프로그래머스/1/12977. 소수 만들기/소수 만들기.cpp
@@ -0,0 +1,38 @@
+#include <vector>
+#include <iostream>
+using namespace std;
+
+// 소수 여부를 확인하는 함수
+bool isPrime(int n){
+    int answer = 0;
+    for(int i=1;i<=n;i++){
+        if(n%i==0) answer++;
+    }
+    if(answer==2) return true;
+    else return false;
+}
+
+int solution(vector<int> nums) {
+    int answer = 0;
+    
+    int length = nums.size();
+    
+    // 주어진 배열 nums에서 3개의 숫자를 골라 더하는 경우의 수
+    int i, j, k;
+    int sum;
+    
+    for(i=0;i<length-2;i++){
+        sum = 0;
+        for(j=i+1;j<length-1;j++){
+            for(k=j+1;k<length;k++){
+                sum = nums[i]+nums[j]+nums[k];
+                if(isPrime(sum)) answer++;
+            }
+        }
+    }
+
+    // [실행] 버튼을 누르면 출력 값을 볼 수 있습니다.
+    // cout << "Hello Cpp" << endl;
+
+    return answer;
+}
