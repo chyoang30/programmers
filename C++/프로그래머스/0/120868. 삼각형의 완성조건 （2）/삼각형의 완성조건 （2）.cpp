4cba342267d435cfa7e9e29c4224751229706fbf
Hyeyeon Kim <132979277+chyoang30@users.noreply.github.com>
2026-05-15 22:12:22 +0900

    Simplify triangle condition calculation
    Refactor triangle condition logic to simplify calculation of possible third side lengths.

--- C++/프로그래머스/0/120868. 삼각형의 완성조건 （2）/삼각형의 완성조건 （2）.cpp
+++ C++/프로그래머스/0/120868. 삼각형의 완성조건 （2）/삼각형의 완성조건 （2）.cpp
@@ -8,15 +8,13 @@ int solution(vector<int> sides) {
     int answer = 0;
     sort(sides.begin(), sides.end()); // 정렬해서 인덱스 0이 짧은 변, 1이 긴 변으로
     
-    // case 1: 가장 긴 변이 sides[1]인 경우, sides[0]+i > sides[1]
-    for(int i=sides[1];i+sides[0]>sides[1];i--){
-        answer++;
-    }
+    /*
+    주어진 두 변이 a, b(a<b)일 때 나머지 변 c를 구하는 문제
+    b-a < c < a+b
     
-    // case 2: 가장 긴 변이 sides에 없는 경우
-    for(int i=sides[1]+1;i<sides[0]+sides[1];i++){
-        answer++;
-    }
+    c의 개수 = (a+b)-(b-a)-1 = 2a-1
+    */
     
-    return answer;
+    return 2*sides[0]-1;
+    
 }
